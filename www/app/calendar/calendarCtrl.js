a9d8eacfc8c403f4b6158bc195646779e453b562
Melroy Dmello <m_dmello@live.com>
2017-10-16 01:01:46 -0700

    implemented distance matrix api

--- www/app/calendar/calendarCtrl.js
+++ www/app/calendar/calendarCtrl.js
@@ -5,28 +5,32 @@
     angular.module('Calendar', []);
     angular.module('Calendar').controller('CalendarCtrl', CalendarCtrl);
 
-    CalendarCtrl.$inject = ['CalendarService', 'calendarConfig', 'profileService'];
+    CalendarCtrl.$inject = ['CalendarService', 'calendarConfig', 'profileService', '$scope'];
 
-    function CalendarCtrl(CalendarService, calendarConfig, profileService) {
+    function CalendarCtrl(CalendarService, calendarConfig, profileService, $scope) {
         var vm = this;
         vm.Users = [];
         vm.calendarView = "month";
         vm.selectedPriorLocation = "home";
         vm.viewDate = new Date();
-        
+        vm.travelModeArray = [];
+        vm.displayTravelModes = false;
+
         //variables for work address
         vm.meetingAutocomplete = '';
         vm.meetingLocationDetails = {};
         vm.initialAddress = profileService.currentUserLocation ? profileService.currentUserLocation : profileService.getCurrentUserLocation();
-                
+        
+        vm.changePriorLocation = changePriorLocation;
+
         $('#meetingStart').datetimepicker({
-            allowInputToggle : true,
-            sideBySide : true
+            allowInputToggle: true,
+            sideBySide: true
         });
         $('#meetingEnd').datetimepicker({
             useCurrent: false,
-            allowInputToggle : true,
-            sideBySide : true
+            allowInputToggle: true,
+            sideBySide: true
         });
         $("#meetingStart").on("dp.change", function (e) {
             $('#meetingEnd').data("DateTimePicker").minDate(e.date);
@@ -46,5 +50,52 @@
                 startsAt: moment().startOf('month').add(1, 'day').toDate()
             }
         ];
+        
+        function changeLocation(){
+            vm.displayTravelModes = false;
+            vm.selectedTravelMode = null;
+            if(!vm.originPlaceId){
+                vm.originPlaceId = profileService.homeLocation.place_id;
+            }
+            CalendarService.fetchTransitDetails(vm.originPlaceId, vm.destinationPlaceId).then(function(data){
+                vm.travelModeArray = data;
+                vm.displayTravelModes = true;
+            })
+        }
+
+        $scope.$watch(function () {
+            return vm.meetingLocationDetails.place_id;
+        }, function (newValue) {
+            if(newValue){
+                vm.destinationPlaceId = newValue;
+                changeLocation();  
+            }
+        });
+        
+        $scope.$watch(function () {
+            return vm.otherLocationDetails.place_id;
+        }, function (newValue) {
+            if(newValue){
+                vm.originPlaceId = newValue;
+                changeLocation();  
+            }
+        });
+        
+        function changePriorLocation(){
+            vm.displayTravelModes = false;
+            vm.selectedTravelMode = null;
+            vm.otherLocation = null;
+            vm.otherLocationDetails = null;
+            switch(vm.selectedPriorLocation){
+                case "home" :
+                    vm.originPlaceId = profileService.homeLocation.place_id;
+                    changeLocation();
+                    break;
+                case "work" :
+                    vm.originPlaceId = profileService.workLocation.place_id;
+                    changeLocation();
+                    break;     
+            }
+        }
     }
 })();
--- www/app/calendar/calendarService.js
+++ www/app/calendar/calendarService.js
@@ -1,14 +1,61 @@
+/*global angular, GOOGLE_API_KEY, $*/
+
 (function () {
     angular.module('Calendar').service('CalendarService', CalendarService);
 
-    CalendarService.$inject = ['centralAPIService', '$rootScope'];
+    CalendarService.$inject = ['centralAPIService', '$rootScope', '$http', '$q'];
 
-    function CalendarService(centralAPIService, $rootScope) {
+    function CalendarService(centralAPIService, $rootScope, $http, $q) {
         var vm = this;
-        vm.fetchDummyAPIDetails = fetchDummyAPIDetails;
+        vm.fetchTransitDetails = fetchTransitDetails;
 
-        function fetchDummyAPIDetails() {
-            return centralAPIService.callAPI("calendar", {}, "post");
+        function fetchTravelModeDetails(travelMode) {
+            var deferredObject = $q.defer();
+            $http({
+                method: 'GET',
+                url: "https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=place_id:" + vm.originPlaceId + "&destinations=place_id:" + vm.destinationPlaceId + "&mode=" + travelMode + "&key=" + GOOGLE_API_KEY + "&callback",
+                headers: {
+                    'Authorization': undefined
+                }
+            }).then(function successCallback(response) {
+                deferredObject.resolve(response);
+            }, function errorCallback() {});
+            return deferredObject.promise;
+        }
+
+
+        function fetchTransitDetails(originPlaceId, destinationPlaceId) {
+            var deferredObject = $q.defer();
+            vm.originPlaceId = originPlaceId;
+            vm.destinationPlaceId = destinationPlaceId;
+            $.when(fetchTravelModeDetails("driving"), fetchTravelModeDetails("walking"), fetchTravelModeDetails("bicycling"), fetchTravelModeDetails("transit"))
+                .then(function (val1, val2, val3, val4) {
+                    var travelModeArray = [];
+                    travelModeArray.push({
+                        mode : "driving",
+                        icon : "car",
+                        value : val1.data.rows[0].elements[0]
+                    });
+                    travelModeArray.push({
+                        mode : "walking",
+                        icon : "male",
+                        value : val2.data.rows[0].elements[0]
+                    })
+                    travelModeArray.push({
+                        mode : "bicycling",
+                        icon : "bicycle",
+                        value : val3.data.rows[0].elements[0]
+                    })
+                    travelModeArray.push({
+                        mode : "transit",
+                        icon : "bus",
+                        value : val4.data.rows[0].elements[0]
+                    })
+                    deferredObject.resolve(travelModeArray);
+                })
+                .fail(function () {
+                })
+            return deferredObject.promise;
         }
     }
 })();
